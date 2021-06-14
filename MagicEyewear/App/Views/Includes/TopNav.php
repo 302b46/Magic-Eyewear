fdafe4066ae1b93c492ef03da7c394ecd2c8125f
RagaaM <ragaamoustaffa@gmail.com>
2021-06-14 22:15:50 +0200

    approot w index

--- MagicEyewear/App/Views/Includes/TopNav.php
+++ MagicEyewear/App/Views/Includes/TopNav.php
@@ -1,90 +1,187 @@
-<html>
+
+<html lang="en">
+
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="robots" content="all,follow">
-     <!-- Bootstrap CSS-->
-    <link rel="stylesheet" href="/Magic-Eyewear/MagicEyewear/Public/vendor/bootstrap/css/bootstrap.min.css">
-    <!-- Font Awesome CSS-->
-    <link rel="stylesheet" href="/Magic-Eyewear/MagicEyewear/Public/vendor/font-awesome/css/font-awesome.min.css">
-    
-    <!-- Google fonts - Roboto-->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,700">
-   <!-- theme stylesheet-->
-    <link rel="stylesheet" href="/Magic-Eyewear/MagicEyewear/Public/CSS/TopNav.css" id="theme-stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    <link rel="stylesheet" type="text/css" href="./css/reset.min.css">
+    <link rel="stylesheet" type="text/css" href="./css/style.min.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+
 </head>
+
 <body>
-<div id="all">
-    <header class="nav-holder make-sticky">
-        <div id="navbar" class="navbar navbar-expand-lg" role="navigation" >
-            <div class="container">
-                <a href="/MagicEyewear/Public/Index.php" class="navbar-brand home">
-                <img src="/MagicEyewear/Public/Images/eyecare-logo.png" alt="EyeCare Optics logo" class="d-none d-md-inline-block"></a>
-                <button type="button" data-toggle="collapse" data-target="#navigation" class="navbar-toggler btn-template-outlined">
-                    <span class="sr-only">Toggle navigation</span> <i class="fa fa-align-justify"></i></button>
-                <div id="navigation" class="navbar-collapse collapse">
-                <ul class="nav navbar-nav ml-auto">
-                <li class="nav-item dropdown menu-large">
-                    <a href="#" data-toggle="dropdown" class="dropdown-toggle">Optical Glasses<b class="caret"></b></a>
-                    <ul class="dropdown-menu megamenu">
-
+    <!-- Section: Header -->
+    <header class="header">
+        <div class="container">
+            <div class="wrapper">
+                <div class="header-item-left">
+                    <!-- <h1><a href="#" class="brand">Store</a></h1> --> <!-- LOGO 3LA LEFT -->
+                    <ul> 
+                        <a href="#"><img src="asset/eyecare-logo.png" style="margin:auto; " alt="logo" /></a>
                     </ul>
-                </li>
-                <li class="nav-item dropdown menu-large">
-                    <a href="#" data-toggle="dropdown" class="dropdown-toggle">Sunglasses<b class="caret"></b></a>
-                    <ul class="dropdown-menu megamenu">
-                        
-                    </ul>
-                </li>
-                <li class="nav-item dropdown menu-large">
-                    <a href="#" data-toggle="dropdown" class="dropdown-toggle">Sports<b class="caret"></b></a>
-                    <ul class="dropdown-menu megamenu">
-                        
-                    </ul>
-                </li>
-                <li><a href="">About Us</a></li>
-                <li><a href="">Frames Guide</a></li>
-                <li><a href="/Magic-Eyewear/MagicEyewear/App/View/User/LoginRegisterForm.php">Login</a></li>
-
-                <!-- SHOW WHEN CLIENT IS LOGGED IN -->
-                    <!-- <li><a href="">Profile</a></li>
-                    <li><a href="">Cart</a></li>
-                    <li><a href="">Logout</a></li> -->
-
-
-                </ul>
                 </div>
-            </div>            
+                <!-- Section: Navbar Menu -->
+                <div class="header-item-center">
+                    <div class="overlay"></div>
+                    <nav class="menu">
+                        <div class="menu-mobile-header">
+                            <button type="button" class="menu-mobile-arrow"><i class="fas fa-arrow-circle-left"></i></button>
+                            <div class="menu-mobile-title"></div>
+                            <button type="button" class="menu-mobile-close"><i class="far fa-window-close"></i></button>
+                        </div>
+                        <!-- LOGO FO2 AL MENU 
+                        <ul> 
+                        <a href="#"><img src="asset/eyecare-logo.png" style="margin:auto; " alt="logo" /></a>
+                        <br>
+                        </ul>
+                        -->
+                        <ul class="menu-section">
+                            <!-- <li><a href="#">Home</a></li> -->
+                            
+                            <li class="menu-item-has-children">
+                                <a href="#">Medical Glasses <i class="fas fa-chevron-down"></i></a>
+                                <div class="menu-subs menu-mega menu-column-4">
+                                    <div class="list-item">
+                                        <h4 class="title">Shop By Age Group</h4>
+                                        <ul>
+                                            <li><a href="#">Shop Women</a></li>
+                                            <li><a href="#">Shop Men</a></li>
+                                            <li><a href="#">Shop Girls</a></li>
+                                            <li><a href="#">Shop Boys</a></li>
+                                        </ul>
+                                        
+                                    </div>
+                                    
+                                    <div class="list-item">
+                                        <img src="./asset/image-5.jpg" class="responsive" alt="Shop Product">
+                                    </div>
+                                    <div class="list-item">
+                                        <img src="./asset/image-5.jpg" class="responsive" alt="Shop Product">
+                                    </div>
+                                    <div class="list-item">
+                                        <img src="./asset/image-5.jpg" class="responsive" alt="Shop Product">
+                                    </div>
+                                </div>
+                            </li>
 
 
+                            <li class="menu-item-has-children">
+                                <a href="#">Sunglasses <i class="fas fa-chevron-down"></i></a>
+                                <div class="menu-subs menu-mega menu-column-4">
+                                    <div class="list-item">
+                                        <h4 class="title">Shop By Age Group</h4>
+                                        <ul>
+                                            <li><a href="#">Shop Women</a></li>
+                                            <li><a href="#">Shop Men</a></li>
+                                            <li><a href="#">Shop Girls</a></li>
+                                            <li><a href="#">Shop Boys</a></li>
+                                        </ul>
+                                    </div>
+
+                                    <div class="list-item">
+                                        <img src="./asset/image-5.jpg" class="responsive" alt="Shop Product">
+                                    </div>
+                                    <div class="list-item">
+                                        <img src="./asset/image-5.jpg" class="responsive" alt="Shop Product">
+                                    </div>
+                                    <div class="list-item">
+                                        <img src="./asset/image-5.jpg" class="responsive" alt="Shop Product">
+                                    </div>
+                                </div>
+                            </li>
 
 
-        </div> <!-- End of navbar div -->
+                            <li class="menu-item-has-children">
+                                <a href="#">Sports <i class="fas fa-chevron-down"></i></a>
+                                <div class="menu-subs menu-mega menu-column-4">
+                                    <div class="list-item">
+                                        <h4 class="title">Shop By Age Group</h4>
+                                        <ul>
+                                            <li><a href="#">Shop Women</a></li>
+                                            <li><a href="#">Shop Men</a></li>
+                                            <li><a href="#">Shop Girls</a></li>
+                                            <li><a href="#">Shop Boys</a></li>
+                                        </ul>
+                                    </div>
+
+                                    <div class="list-item">
+                                        <img src="./asset/image-5.jpg" class="responsive" alt="Shop Product">
+                                    </div>
+                                    <div class="list-item">
+                                        <img src="./asset/image-5.jpg" class="responsive" alt="Shop Product">
+                                    </div>
+                                    <div class="list-item">
+                                        <img src="./asset/image-5.jpg" class="responsive" alt="Shop Product">
+                                    </div>
+                                </div>
+                            </li>
+
+                            <li class="menu-item-has-children">
+                                <a href="#">Brands <i class="fas fa-chevron-down"></i></a>
+                                <div class="menu-subs menu-mega menu-column-4">
+                                    <div class="list-item text-center">
+                                        <a href="#">
+                                            <img src="./asset/image-1.jpg" class="responsive" alt="brandimg">
+                                            <h4 class="title">Brand Name 1</h4>
+                                        </a>
+                                    </div>
+                                    <div class="list-item text-center">
+                                        <a href="#">
+                                            <img src="./asset/image-2.jpg" class="responsive" alt="brandimg">
+                                            <h4 class="title">Brand Name 2</h4>
+                                        </a>
+                                    </div>
+                                    <div class="list-item text-center">
+                                        <a href="#">
+                                            <img src="./asset/image-3.jpg" class="responsive" alt="brandimg">
+                                            <h4 class="title">Brand Name 3</h4>
+                                        </a>
+                                    </div>
+                                    <div class="list-item text-center">
+                                        <a href="#">
+                                            <img src="./asset/image-4.jpg" class="responsive" alt="New Product">
+                                            <h4 class="title">Brand Name 4</h4>
+                                        </a>
+                                    </div>
+                                </div>
+                            </li>
 
 
+                            <li><a href="#">Frames Guide</a></li>
+                            <li><a href="#">About Us</a></li>
+                        </ul>
+                    </nav>
+                </div>
 
+                <div class="header-item-right">
+                    <a href="#" class="menu-icon"><i class="fas fa-search"></i></a>
+                    <a href="#" class="menu-icon"><i class="fas fa-sign-in-alt"></i></a>
+                    <a href="#" class="menu-icon"><i class="fas fa-shopping-cart"></i></a>
+                    
+                    <button type="button" class="menu-mobile-trigger">
+                        <span></span>
+                        <span></span>
+                    </button>
+                </div>
 
-
-
-
-
+            </div>
+        </div>
     </header>
 
+    <!-- Section: Main -->
+    <main class="main">
+        <div class="container">
 
-
-
-
-
-
-
-
-
-
-</div> <!-- End of first div -->
-
- <!-- Javascript files for toggle menu-->
-    <script src="/Magic-Eyewear//MagicEyewear/Public/vendor/jquery/jquery.min.js"></script>
-    <script src="/Magic-Eyewear//MagicEyewear/Public/vendor/bootstrap/js/bootstrap.min.js"></script>
+        </div>
+    </main>
     
 
+
+    
+<script src="./js/script.js" defer></script> 
+    
 </body>
 </html>
