9b5f956425ddd0193dd7bf971fa8ab96789fe790
FaridaHesham99 <faridamohamed1802642@miuegypt.edu.eg>
2021-06-17 05:06:08 +0200

    Create productPage.php

---
+++ MagicEyewear/App/views/defaultPages/productPage.php
@@ -0,0 +1,154 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Product</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="all,follow">
+    <!-- Bootstrap CSS-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Font Awesome CSS-->
+    <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
+    <!-- Google fonts - Roboto-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,700">
+    <!-- Bootstrap Select-->
+    <link rel="stylesheet" href="vendor/bootstrap-select/css/bootstrap-select.min.css">
+    <!-- owl carousel-->
+    <link rel="stylesheet" href="vendor/owl.carousel/assets/owl.carousel.css">
+    <link rel="stylesheet" href="vendor/owl.carousel/assets/owl.theme.default.css">
+    <!-- theme stylesheet-->
+    <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
+    <!-- Custom stylesheet - for your changes-->
+    <link rel="stylesheet" href="CSS/ProductPage.css">
+    <!-- Friconix icons-->
+    <script defer src="https://friconix.com/cdn/friconix.js"> </script>
+    <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+  </head>
+  <body>
+    <div id="all">
+      
+      <div id="heading-breadcrumbs">
+        <div class="container">
+          <div class="row d-flex align-items-center flex-wrap">
+            <div class="col-md-7">
+              <h1 class="h2">White Blouse Armani</h1>
+            </div>
+            <div class="col-md-5">
+              <ul class="breadcrumb d-flex justify-content-end">
+                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                <li class="breadcrumb-item"><a href="shop-category.html">Ladies</a></li>
+                <li class="breadcrumb-item"><a href="shop-category.html">Tops</a></li>
+                <li class="breadcrumb-item active">White Blouse Armani</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="content">
+        <div class="container">
+          <div class="row bar">
+            <!-- LEFT COLUMN _________________________________________________________-->
+            <div class="col-lg-9">
+              <p class="lead">Built purse maids cease her ham new seven among and. Pulled coming wooded tended it answer remain me be. So landlord by we unlocked sensible it. Fat cannot use denied excuse son law. Wisdom happen suffer common the appear ham beauty her had. Or belonging zealously existence as by resources.</p>
+              <p class="goToDescription"><a href="#details" class="scroll-to text-uppercase">Scroll to product details, material & care and sizing</a></p>
+              <div id="productMain" class="row">
+                <div class="col-sm-6">
+                  <div data-slider-id="1" class="owl-carousel shop-detail-carousel">
+                    <div> <img src="Images\products\Diva\4147 40 C829 A.jpg" alt="" class="img-fluid"></div>
+                    <div> <img src="Images\products\Diva\4147 40 C829 F.jpg" alt="" class="img-fluid"></div>
+                    
+                  </div>
+                  <div data-slider-id="1" class="owl-thumbs">
+                    <button class="owl-thumb-item"><img src="Images\products\Diva\4147 40 C829 A.jpg" alt="" class="img-fluid"></button>
+                    <button class="owl-thumb-item"><img src="Images\products\Diva\4147 40 C829 F.jpg" alt="" class="img-fluid"></button>
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="box">
+                    <form>
+                      <div class="sizes">
+                        <h3>Diva Sunglasses</h3>
+                       
+                      </div>
+                      <p class="price">$124.00</p>
+                      <p class="text-center">
+                        <button type="submit" class="btn btn-template-outlined"><i class="fi-xnsuxl-camera-solid"></i> Try-On</button><br><br>
+                        <button type="submit" class="btn btn-template-outlined"><i class="fa fa-shopping-cart"></i> Add to cart</button>
+                      </p>
+                    </form>
+                  </div>
+
+                </div>
+              </div>
+              <div id="details" class="box mb-4 mt-4">
+                <p></p>
+                <h4>Product details</h4>
+                <p>White lace top, woven, has a round neck, short sleeves, has knitted lining attached</p>
+                <h4>Material & care</h4>
+                <ul>
+                  <li>Polyester</li>
+                  <li>Machine wash</li>
+                </ul>
+                <h4>Size & Fit</h4>
+                <ul>
+                  <li>Regular fit</li>
+                  <li>The model (height 5'8 "and chest 33") is wearing a size S</li>
+                </ul>
+                <blockquote class="blockquote">
+                  <p class="mb-0"><em>Define style this season with Armani's new range of trendy tops, crafted with intricate details. Create a chic statement look by teaming this lace number with skinny jeans and pumps.</em></p>
+                </blockquote>
+              </div>
+              
+              <!-- <div class="row">
+                <div class="col-lg-3 col-md-6">
+                  <div class="box text-uppercase mt-0 mb-2">
+                    <h3>You may also like these products</h3>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                  <div class="product">
+                    <div class="image"><a href="shop-detail.html"><img src="Images\products\Diva\4161 40 C97A  A.jpg" alt="" class="img-fluid image1"></a></div>
+                    <div class="text">
+                      <h3 class="h5"><a href="shop-detail.html">Diva</a></h3>
+                      <p class="price">$143</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                  <div class="product">
+                    <div class="image"><a href="shop-detail.html"><img src="Images\products\Diva\4147 40 C829 A.jpg" alt="" class="img-fluid image1"></a></div>
+                    <div class="text">
+                      <h3 class="h5"><a href="shop-detail.html">Diva</a></h3>
+                      <p class="price">$143</p>
+                    </div>
+                  </div>
+                </div>
+               
+              </div> -->
+            </div>
+            
+          </div>
+        </div>
+      </div>
+ 
+    </div>
+    <!-- Javascript files-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper.js/umd/popper.min.js"> </script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="vendor/jquery.cookie/jquery.cookie.js"> </script>
+    <script src="vendor/waypoints/lib/jquery.waypoints.min.js"> </script>
+    <script src="vendor/jquery.counterup/jquery.counterup.min.js"> </script>
+    <script src="vendor/owl.carousel/owl.carousel.min.js"></script>
+    <script src="vendor/owl.carousel2.thumbs/owl.carousel2.thumbs.min.js"></script>
+    <script src="js/jquery.parallax-1.1.3.js"></script>
+    <script src="vendor/bootstrap-select/js/bootstrap-select.min.js"></script>
+    <script src="vendor/jquery.scrollto/jquery.scrollTo.min.js"></script>
+    <script src="js/front.js"></script>
+  </body>
+</html>
