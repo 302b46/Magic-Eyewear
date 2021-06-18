f1dce3735beff458cf65a521c0940ae7cf28b34b
FaridaHesham99 <faridamohamed1802642@miuegypt.edu.eg>
2021-06-18 23:13:44 +0200

    Cart Final Design

--- MagicEyewear/App/views/client/cart.php
+++ MagicEyewear/App/views/client/cart.php
@@ -6,25 +6,20 @@
     <title><?=$data['page_title'] . " | " . WEBSITE_TITLE?></title>
     <link rel="stylesheet" href="<?=ASSETS?>css/cart.css">
 <!-- Bootstrap CDN -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    
-    <link rel="stylesheet" href="https://mdbootstrap.com/previews/ecommerce-demo/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://mdbootstrap.com/previews/ecommerce-demo/css/mdb-pro.min.css">
-    
-    <link rel="stylesheet" href="https://mdbootstrap.com/previews/ecommerce-demo/css/mdb.ecommerce.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script defer src="https://friconix.com/cdn/friconix.js"> </script>
 
   </head>
   <body class="skin-light">
     <!--Section: Block Content-->
-  <section>
+  <section class="cart"> 
 <div class="container">
 
     <!--Grid row-->
     <div class="row">
 
       <!--Grid column-->
-      <div class="col-lg-5">
+      <div class="shadow p-3 mb-5 bg-white rounded col-lg-6" style="padding-right:100px; float:left;">
 
         <!-- Card -->
         <div class="mb-2">
@@ -32,37 +27,23 @@
 
             <h5 class="mb-4">Cart (<span>2</span> items)</h5>
 
-            <div class="row mb-4">
+            <div class="row mb-4" id="cartContent">
               <div class="col-md-5 col-lg-3 col-xl-3">
                 <div class="view zoom overlay z-depth-1 rounded mb-3 mb-md-0">
-                  <img class="img-fluid w-100"
-                    src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12a.jpg" alt="Sample">
-                  <a href="#!">
-                    <div class="mask">
-                      <img class="img-fluid w-100"
-                        src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12.jpg">
-                      <div class="mask rgba-black-slight"></div>
-                    </div>
-                  </a>
+                  <img class="imgCart"
+                    src='<?=ASSETS?>Images/DIVA sunglasses/4162 40 C97AS A.jpg' alt="Sample">
+                  
                 </div>
               </div>
               <div class="col-md-7 col-lg-9 col-xl-9">
                 <div>
                   <div class="d-flex justify-content-between">
-                    <div>
-                      <h5>Blue denim shirt</h5>
-                      <p class="mb-3 text-muted text-uppercase small">Shirt - blue</p>
-                      <p class="mb-2 text-muted text-uppercase small">Color: blue</p>
-                      <p class="mb-3 text-muted text-uppercase small">Size: M</p>
+                    <div >
+                      <h5>DIVA Sunglasses</h5>
+                      <p class="mb-3 text-muted text-uppercase small">Model</p>
+                      <p class="mb-2 text-muted text-uppercase small">Color: Black</p>
                     </div>
-                    <div>
-                      <div class="def-number-input number-input safari_only mb-0 w-100">
-                        <button onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
-                          class="minus decrease"></button>
-                        <input class="quantity" min="0" name="quantity" value="1" type="number">
-                        <button onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
-                          class="plus increase"></button>
-                      </div>
+                    
                       
                     </div>
                   </div>
@@ -71,7 +52,17 @@
                       <a href="#!" type="button" class="card-link-secondary small text-uppercase mr-3"><i class="fi-xnsuxl-trash-solid"></i> Remove item </a>
                       
                     </div>
-                    <p class="mb-0"><span><strong id="summary">$17.99</strong></span></p class="mb-0">
+                    <p class="mb-0"><span><strong id="summary">1,780 EGP</strong></span></p class="mb-0">
+                    <div class="quantity">
+                      <form action="">
+                          <select name="quantity" id="quantity" class="quantity">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                          </select>
+                        </form>
+                      </div>
                   </div>
                 </div>
               </div>
@@ -81,41 +72,37 @@
               <div class="col-md-5 col-lg-3 col-xl-3">
                 <div class="view zoom overlay z-depth-1 rounded mb-3 mb-md-0">
                   <img class="img-fluid w-100"
-                    src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg" alt="Sample">
-                  <a href="#!">
-                    <div class="mask">
-                      <img class="img-fluid w-100"
-                        src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13.jpg">
-                      <div class="mask rgba-black-slight"></div>
-                    </div>
-                  </a>
+                    src='<?=ASSETS?>Images/DIVA sunglasses/4165 40 C289 A.jpg' alt="Sample">
+                  
                 </div>
               </div>
               <div class="col-md-7 col-lg-9 col-xl-9">
                 <div>
                   <div class="d-flex justify-content-between">
                     <div>
-                      <h5>Red hoodie</h5>
-                      <p class="mb-3 text-muted text-uppercase small">Shirt - red</p>
-                      <p class="mb-2 text-muted text-uppercase small">Color: red</p>
-                      <p class="mb-3 text-muted text-uppercase small">Size: M</p>
+                      <h5>DIVA Sunglasses</h5>
+                      <p class="mb-3 text-muted text-uppercase small">Model</p>
+                      <p class="mb-2 text-muted text-uppercase small">Color: Brown</p>
+                    
                     </div>
-                    <div>
-                      <div class="def-number-input number-input safari_only mb-0 w-100">
-                        <button onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
-                          class="minus"></button>
-                        <input class="quantity" min="0" name="quantity" value="1" type="number">
-                        <button onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
-                          class="plus"></button>
-                      </div>
-                    </div>
+                   
                   </div>
                   <div class="d-flex justify-content-between align-items-center">
                     <div>
                       <a href="#!" type="button" class="card-link-secondary small text-uppercase mr-3"><i class="fi-xnsuxl-trash-solid"></i> Remove item </a>
                       
                     </div>
-                    <p class="mb-0"><span><strong>$35.99</strong></span></p class="mb-0">
+                    <p class="mb-0"><span><strong>1,500 EGP</strong></span></p class="mb-0">
+                    <div class="quantity">
+                      <form action="">
+                          <select name="quantity" id="quantity" class="quantity">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                          </select>
+                        </form>
+                      </div>
                   </div>
                 </div>
               </div>
@@ -126,45 +113,48 @@
         </div>
        
 
-        
+        <!--Grid column-->
+      <div class="shadow p-3 mb-5 bg-white rounded col-lg-4" style=" float:right; margin-left:2%; height:280px; ">
 
-      </div>
-      <!--Grid column-->
+<!-- Card -->
+<div class="mb-3">
+  <div class="pt-4">
 
-      <!--Grid column-->
-      <div class="col-lg-4">
+    <h5 class="mb-3">Order Summary:</h5>
 
-        <!-- Card -->
-        <div class="mb-3">
-          <div class="pt-4">
-
-            <h5 class="mb-3">The Total Amount of:</h5>
-
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
-                Temporary amount
-                <span>$25.98</span>
-              </li>
-              
-              <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
-                <div>
-                  <strong>The total amount of</strong>
-                  <strong>
-                    <p class="mb-0">(including VAT)</p>
-                  </strong>
-                </div>
-                <span><strong>$53.98</strong></span>
-              </li>
-            </ul>
-
-            <button type="button" class="btn btn-primary btn-block" style="font-size:15px;">Reserve Products</button>
-
-          </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
+        1x DIVA Sunglasses
+        <span>1,780 EGP</span>
+        <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
+        1x DIVA Sunglasses
+        <span>1,500 EGP</span>
+      </li>
+      
+      <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
+        <div>
+          <strong>The total amount of</strong>
+          
         </div>
-        <!-- Card -->
+        <span><strong>3,280 EGP</strong></span>
+      </li>
+    </ul>
+    <div class="wrap">
+    <button type="button" class="reserve" style="font-size:15px;">Reserve Products</button>
 
-        
+    </div>
+
+  </div>
+</div>
+<!-- Card -->
+
+
+</div>
+
       </div>
+      <!--Grid column-->
+
+      
       <!--Grid column-->
 
     </div>
