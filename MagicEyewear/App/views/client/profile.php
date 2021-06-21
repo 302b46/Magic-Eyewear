ca867784bd6c499ff90cc21f79fcc9bb14799ade
FaridaHesham99 <faridamohamed1802642@miuegypt.edu.eg>
2021-06-22 00:32:20 +0200

    Update profile.php

--- MagicEyewear/App/views/client/profile.php
+++ MagicEyewear/App/views/client/profile.php
@@ -9,10 +9,10 @@
     <link rel="stylesheet" href="<?=ASSETS?>css/clientProfile.css">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+	<!-- <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.1/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.1/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
 </head>
 <body>
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
@@ -30,9 +30,7 @@
                 
                 <div class="col d-flex flex-column flex-sm-row justify-content-between mb-3">
                   <div class="text-center text-sm-left mb-2 mb-sm-0">
-                    <h4 class="pt-sm-2 pb-1 mb-0 text-nowrap">Welcome, Salma Ahmed!</h4>
-                    <p class="mb-0">@salma22</p>
-                   
+                    <h2 class="pt-sm-2 pb-1 mb-0 text-nowrap">Welcome, <?php echo $_SESSION['firstName'];?>!</h2>
                     
                   </div>
                   
@@ -42,8 +40,8 @@
                 <div>
               <!-- Nav Tabs -->
               <ul class="nav nav-tabs" role="tablist">
-                <li class="nav-item"><a data-toggle="tab" href="" class="active nav-link">Edit Profile</a></li>
-                <li class="nav-item"><a data-toggle="tab" href="" class="nav-link" >Order History</a></li>
+                <li class="nav-item" ><a data-toggle="tab" href="#edit" class="active nav-link" >Edit Profile</a></li>
+                <li class="nav-item" ><a data-toggle="tab" href="#history" class="nav-link" >Order History</a></li>
               </ul>
 
               <!-- Tab Panes -->
@@ -56,13 +54,13 @@
                           <div class="col">
                             <div class="form-group">
                               <label>First Name</label>
-                              <input class="form-control" type="text" name="name" placeholder="Salma" value="Salma">
+                              <input class="form-control" type="text" name="name" placeholder="Salma" value="<?php echo $_SESSION['firstName'];?>">
                             </div>
                           </div>
                           <div class="col">
                             <div class="form-group">
                               <label>Last Name</label>
-                              <input class="form-control" type="text" name="last" placeholder="salma22" value="Ahmed">
+                              <input class="form-control" type="text" name="last" placeholder="salma22" value="<?php echo $_SESSION['lastName'];?>">
                             </div>
                           </div>
                         </div>
@@ -70,7 +68,7 @@
                           <div class="col">
                             <div class="form-group">
                               <label>Email</label>
-                              <input class="form-control" type="text" placeholder="salma@gmail.com" readonly>
+                              <input class="form-control" type="text" value="<?php echo $_SESSION['email'];?>" readonly>
                             </div>
                           </div>
                         </div>
@@ -79,30 +77,12 @@
                     </div>
                     <div class="row">
                       <div class="col-12 col-sm-6 mb-3">
-                        <div class="mb-2"><b>Change Password</b></div>
-                        <div class="row">
-                          <div class="col">
-                            <div class="form-group">
-                              <label>Current Password</label>
-                              <input class="form-control" type="password" >
-                            </div>
-                          </div>
+                        <div class="mb-2">
+                       
+                        <button class="changePass" type="button" data-toggle="modal" data-target="#changePassModal">Change Password</button>
                         </div>
-                        <div class="row">
-                          <div class="col">
-                            <div class="form-group">
-                              <label>New Password</label>
-                              <input class="form-control" type="password" >
-                            </div>
-                          </div>
-                        </div>
-                        <div class="row">
-                          <div class="col">
-                            <div class="form-group">
-                              <label>Confirm <span class="d-none d-xl-inline">Password</span></label>
-                              <input class="form-control" type="password" ></div>
-                          </div>
-                        </div>
+                      
+                       
                       </div>
                       
                     </div>
@@ -110,7 +90,7 @@
                       <div class="col d-flex justify-content-end">
                         <button class="saveChanges" type="button" data-toggle="modal" data-target="#saveModal">Save Changes</button>
                         </div>
-                        <!-- Modal -->
+                        <!-- Save Changes Modal -->
                         <div class="modal fade" tabindex="-1" role="dialog" id="saveModal">
                             <div class="modal-dialog" role="document">
                               <div class="modal-content">
@@ -153,8 +133,6 @@
 </div>
 <?php //$this->view("includes/Footer",$data);?> 
 
-
-<script type="text/javascript"></script>
 <?php $this->view("includes/Footer",$data);?> 
 </body>
 </html>
