e4b7eb5f72031635881abb2a0879b0ec03b3cf67
FaridaHesham99 <faridamohamed1802642@miuegypt.edu.eg>
2021-06-22 03:56:17 +0200

    Client Profile

--- MagicEyewear/App/views/client/profile.php
+++ MagicEyewear/App/views/client/profile.php
@@ -9,10 +9,7 @@
     <link rel="stylesheet" href="<?=ASSETS?>css/clientProfile.css">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
-	<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.1/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
 </head>
 <body>
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
@@ -46,21 +43,24 @@
 
               <!-- Tab Panes -->
               <div class="tab-content pt-3">
+
+          <!-- Beginning of tab Edit Profile -->
+
                 <div class="tab-pane active" id="edit">
-                  <form class="form" novalidate="">
+                  <form class="form" action="">
                     <div class="row">
                       <div class="col">
                         <div class="row">
                           <div class="col">
                             <div class="form-group">
                               <label>First Name</label>
-                              <input class="form-control" type="text" name="name" placeholder="Salma" value="<?php echo $_SESSION['firstName'];?>">
+                              <input class="form-control" type="text" name="firstname" value="<?php echo $_SESSION['firstName'];?>">
                             </div>
                           </div>
                           <div class="col">
                             <div class="form-group">
                               <label>Last Name</label>
-                              <input class="form-control" type="text" name="last" placeholder="salma22" value="<?php echo $_SESSION['lastName'];?>">
+                              <input class="form-control" type="text" name="lastname" value="<?php echo $_SESSION['lastName'];?>">
                             </div>
                           </div>
                         </div>
@@ -68,27 +68,23 @@
                           <div class="col">
                             <div class="form-group">
                               <label>Email</label>
-                              <input class="form-control" type="text" value="<?php echo $_SESSION['email'];?>" readonly>
+                              <input class="form-control" type="text" name="email" value="<?php echo $_SESSION['email'];?>" readonly>
                             </div>
                           </div>
                         </div>
                         
                       </div>
                     </div>
+                    <hr>
+                   
                     <div class="row">
-                      <div class="col-12 col-sm-6 mb-3">
-                        <div class="mb-2">
-                       
+                      
+                        <div class="col d-flex justify-content-end">
                         <button class="changePass" type="button" data-toggle="modal" data-target="#changePassModal">Change Password</button>
                         </div>
-                      
-                       
-                      </div>
-                      
-                    </div>
-                    <div class="row">
-                      <div class="col d-flex justify-content-end">
-                        <button class="saveChanges" type="button" data-toggle="modal" data-target="#saveModal">Save Changes</button>
+                        <div class="col d-flex justify-content-left">
+                        <!-- <button class="saveChanges" type="button" data-toggle="modal" data-target="#saveModal">Save Changes</button> -->
+                        <input type="submit" value="Save Changes" class="saveChanges" name="save">
                         </div>
                         <!-- Save Changes Modal -->
                         <div class="modal fade" tabindex="-1" role="dialog" id="saveModal">
@@ -105,18 +101,59 @@
                                 </div>
                                 <div class="modal-footer">
                                   
-                                  <button type="button" class="closeButton" data-dismiss="modal">Close</button>
-                                  <button type="button" class="save" data-dismiss="modal">Save</button>
+                                  <input type="submit" class="closeButton" data-dismiss="modal" name="close" value="Close">
+                                  <input type="submit" class="save" name="save" value="Save">
+                                  
+                                  
                                 </div>
                               </div>
                             </div>
-                          </div>
+                          </div> 
+                          <!-- End of Save Changes Modal -->
+
+
+
+                          <!-- Change Password Modal -->
+                        <div class="modal fade" tabindex="-1" role="dialog" id="changePassModal">
+                            <div class="modal-dialog" role="document">
+                              <div class="modal-content">
+                                <div class="modal-header">
+                                  <h5 class="modal-title">Change Password</h5>
+                                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                  </button>
+                                </div>
+                                <div class="modal-body">
+                                <div class="form-group">
+                                  <form action="">
+                                      <input type="text" placeholder="Current Password" class="form-control"><br>
+                                      <input type="text" placeholder="New Password" class="form-control"><br>
+                                      <input type="text" placeholder="Confirm Password" class="form-control">
+                                  </form>
+                                </div>
+                                  
+                                </div>
+                                <div class="modal-footer">
+                                  <input type="reset" class="resetBtn" value="Reset" name="reset">
+                                  <input type="submit" class="save" value="Save" name="savePass">
+                                </div>
+                              </div>
+                            </div>
+                          </div> 
+                          <!-- End of Change Password Modal -->
                       
                     </div>
                   </form>
 
                 </div>
-                <div class="tab-pane" id="history"><h2>Order History</h2></div>
+                <!-- End of tab Edit Profile -->
+
+
+                <!-- Beginning of tab Order History -->
+                <div class="tab-pane" id="history">
+                  <h2>Order History</h2>
+                </div>
+                <!-- End of tab Order History -->
               </div>
             </div>
             </div>
@@ -131,7 +168,6 @@
   </div>
 </div>
 </div>
-<?php //$this->view("includes/Footer",$data);?> 
 
 <?php $this->view("includes/Footer",$data);?> 
 </body>
